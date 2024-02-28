63371f3d79c26e84a779670291d7207e5d390e86
Nana Amakye <noamakye@gmail.com>
2024-02-27 23:48:35 -0600

    added a feature for connecting buttons to content

---
+++ interact.js
@@ -0,0 +1,33 @@
+/*When a button is clicked, the corresponding paragraph will be displayed, and all others will be hidden. 
+
+- Targeting buttons in the research questions part
+*/
+
+// Get all the buttons
+const buttons = document.querySelectorAll('.r-button');
+
+// Add event listener to each button
+buttons.forEach((button) => {
+	button.addEventListener('click', function () {
+		// Hide all ul elements
+		document.querySelectorAll('.content-container ul').forEach((ul) => {
+			ul.style.display = 'none';
+		});
+
+		// Get the ul id based on the button id
+		const ulId = this.id.replace('btn-', 'content');
+
+		// Display the corresponding ul
+		document.getElementById(ulId).style.display = 'block';
+
+		// Change the button's background color to indicate it's active
+		this.style.backgroundColor = '#b0c4b1';
+
+		// Reset the background color of all buttons to default
+		buttons.forEach((btn) => {
+			if (btn !== this) {
+				btn.style.backgroundColor = '';
+			}
+		});
+	});
+});
